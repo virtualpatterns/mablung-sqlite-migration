ff8467b4688b89f89303c928c72c5b5f7448dd19
virtualpatterns <code.virtualpatterns@gmail.com>
2020-09-18 13:46:53 -0400

    post-test

--- distributable/test/library/migration/20200823213000-null.js
+++ distributable/test/library/migration/20200823213000-null.js
@@ -4,6 +4,16 @@ class Migration extends BaseMigration {
 
   constructor(path, database) {
     super(path, database);
+  }
+
+  async install() {
+    await this._database.installNull();
+    await super.install();
+  }
+
+  async uninstall() {
+    await this._database.uninstallNull();
+    await super.uninstall();
   }}
 
 
